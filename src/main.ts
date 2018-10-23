c3028f4d6bcc52ab6c57de3fb2a4f9681839ed60
Thinh Phan <thinh.phan.van@preciofishbone.se>
2018-10-23 18:25:08 +0700

    init Vuetify

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
