b989ffe54cb47cc1bbcff84382929296d3a1757d
Nguyen Phan Duc Thinh <ngphanducthinh@gmail.com>
2018-10-24 09:59:31 +0700

    router for leave form

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import LeaveForm from "./views/LeaveForm.vue";
+
 
 Vue.use(Router)
 
@@ -10,12 +12,17 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: Home
     },
     {
+      path: "/leaveform",
+      name: "LeaveForm",
+      component: LeaveForm
+    },
+    {
       path: '/about',
-      name: 'about',
+      name: 'About',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
