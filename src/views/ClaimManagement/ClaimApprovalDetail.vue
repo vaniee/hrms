ea9e03b035eb2c147268d2b2145d0764d9548ffe
Van Nguyen HaTuyet <Van.Nguyen.Ha.Tuyet@preciofishbone.se>
2018-10-26 16:21:44 +0700

    sfdf

--- src/views/ClaimManagement/ClaimApprovalDetail.vue
+++ src/views/ClaimManagement/ClaimApprovalDetail.vue
@@ -147,7 +147,41 @@
         </v-tab-item>
 
         <v-tab-item id ="tab-2">
-          dfg
+          <v-list two-line>
+                  <template v-for="(item, index) in items">
+                    <v-subheader class="subheading"
+                      v-if="item.header"
+                      :key="item.header">
+                      {{ item.header }}
+                    </v-subheader>
+
+                    <v-divider
+                      v-else-if="item.divider"
+                      :inset="item.inset"
+                      :key="index"></v-divider>
+
+                    <v-list-tile
+                      v-else
+                      :key="item.title"
+                      avatar>
+                      <v-list-tile-avatar>
+                        <img :src="item.avatar">
+                      </v-list-tile-avatar>
+                      <v-tooltip v-model="item.showComment" left max-width="200">
+                          <span>{{item.comment}}</span>
+                      </v-tooltip>
+                      <v-list-tile-content>
+                        <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                        <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                        <v-list-tile-sub-title>
+                          <span @click='item.showComment = !item.showComment' class="clickable">View comment</span>
+                        </v-list-tile-sub-title>
+                      </v-list-tile-content>
+
+                      <v-icon color="green darken-2">check</v-icon>
+                    </v-list-tile>
+                  </template>
+                </v-list>
         </v-tab-item>
         </v-tabs-items>
         
